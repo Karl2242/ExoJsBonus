8f91662d21b4d1ca84969f42e6f1035d28a5065d
Karl2242 <kchikhicloud@gmail.com>
2024-11-19 16:25:16 +0100

    exo3

---
+++ Ex 3/script.js
@@ -0,0 +1,15 @@
+let leText = document.getElementById("text");
+
+document.addEventListener("keyup", handleClickOnKeyboard);
+
+
+function handleClickOnKeyboard(event){
+
+if(event.key === "a"){
+
+    console.log("bien detecté")
+
+}
+
+}
+
